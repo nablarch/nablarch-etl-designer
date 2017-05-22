c0bd1423a899f1a8ebd2eb33d5c03b7a6febe1ad
TISNT\tie305231 <yoshinouchi.ryota@tis.co.jp>
2017-05-22 10:36:21 +0900

    Electron上でbpmn-jsが動作する環境構築

---
+++ app/index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var fs = require('fs');
+
+var $ = require('jquery');
+var BpmnModeler = require('bpmn-js/lib/Modeler');
+
+var propertiesPanelModule = require('bpmn-js-properties-panel'),
+    propertiesProviderModule = require('bpmn-js-properties-panel/lib/provider/camunda'),
+    camundaModdleDescriptor = require('camunda-bpmn-moddle/resources/camunda');
+
+var container = $('#js-drop-zone');
+
+var canvas = $('#js-canvas');
+
+var bpmnModeler = new BpmnModeler({
+  container: canvas,
+  propertiesPanel: {
+    parent: '#js-properties-panel'
+  },
+  additionalModules: [
+    propertiesPanelModule,
+    propertiesProviderModule
+  ],
+  moddleExtensions: {
+    camunda: camundaModdleDescriptor
+  }
+});
+
+  bpmnModeler.createDiagram(function(err) {
+
+    if (err) {
+      container
+        .removeClass('with-diagram')
+        .addClass('with-error');
+
+      container.find('.error pre').text(err.message);
+
+      console.error(err);
+    } else {
+      container
+        .removeClass('with-error')
+        .addClass('with-diagram');
+    }
+
+
+  });
