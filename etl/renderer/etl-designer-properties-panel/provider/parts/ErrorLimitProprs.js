ca9fef5f271dbd942a75248c7a98fab0b2496f31
r-nouchi <yoshinouchi.ryota@tis.co.jp>
2017-11-06 10:50:27 +0900

    Add integer validation.

--- etl/renderer/etl-designer-properties-panel/provider/parts/ErrorLimitProprs.js
+++ etl/renderer/etl-designer-properties-panel/provider/parts/ErrorLimitProprs.js
@@ -1,16 +1,34 @@
 'use strict';
 
 var entryFactory = require('bpmn-js-properties-panel/lib/factory/EntryFactory');
+var cmdHelper = require('bpmn-js-properties-panel/lib/helper/CmdHelper');
 
 var is = require('bpmn-js/lib/util/ModelUtil').is;
+var getBusinessObject = require('bpmn-js/lib/util/ModelUtil').getBusinessObject;
 
 module.exports = function(group, element) {
   if (is(element, 'jsr352:Step')) {
-    group.entries.push(entryFactory.textField({
+    group.entries.push(entryFactory.validationAwareTextField({
       id : 'errorLimit',
       description : '',
       label : 'error limit',
-      modelProperty : 'errorLimit'
+      modelProperty : 'errorLimit',
+      getProperty: function(element) {
+        return getBusinessObject(element)['errorLimit'];
+      },
+      setProperty: function(element, properties) {
+        return cmdHelper.updateProperties(element, properties);
+      },
+      validate: function(element, values) {
+        var isValid;
+        if(values['errorLimit']){
+          isValid = /~[0-9]+$/.test(values['errorLimit']);
+        }else if(typeof values['errorLimit'] === 'undefined'){
+          isValid = true;
+        }
+
+        return isValid ? {} : {'errorLimit': 'Must be integer'};
+      }
     }));
   }
 };
--- etl/renderer/etl-designer-properties-panel/provider/parts/UpdateSizeProps.js
+++ etl/renderer/etl-designer-properties-panel/provider/parts/UpdateSizeProps.js
@@ -1,16 +1,34 @@
 'use strict';
 
 var entryFactory = require('bpmn-js-properties-panel/lib/factory/EntryFactory');
+var cmdHelper = require('bpmn-js-properties-panel/lib/helper/CmdHelper');
 
 var is = require('bpmn-js/lib/util/ModelUtil').is;
+var getBusinessObject = require('bpmn-js/lib/util/ModelUtil').getBusinessObject;
 
 module.exports = function(group, element) {
   if (is(element, 'jsr352:Step')) {
-    group.entries.push(entryFactory.textField({
+    group.entries.push(entryFactory.validationAwareTextField({
       id : 'updateSize',
       description : '',
       label : 'commit interval',
-      modelProperty : 'updateSize'
+      modelProperty : 'updateSize',
+      getProperty: function(element) {
+        return getBusinessObject(element)['updateSize'];
+      },
+      setProperty: function(element, properties) {
+        return cmdHelper.updateProperties(element, properties);
+      },
+      validate: function(element, values) {
+        var isValid;
+        if(values['updateSize']){
+          isValid = /^[0-9]+$/.test(values['updateSize']);
+        }else if(typeof values['updateSize'] === 'undefined'){
+          isValid = true;
+        }
+
+        return isValid ? {} : {'updateSize': 'Must be integer'};
+      }
     }));
   }
 };
